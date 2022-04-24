1a237125a6efaf397f2cddf9e017498bf12557c5
Les Aker <me@lesaker.org>
2022-04-24 01:39:39 -0400

    crazy changes all at once

--- manifests/guest.pp
+++ manifests/guest.pp
@@ -2,10 +2,14 @@
 #
 # @param image sets the system image to use to bootstrap the guest
 # @param disks sets the LVs to create for the guest
+# @param ram sets the memory given to the VM
+# @param cores sets the CPU core count given to the VM
 # @param vm_name sets the name of the VM
 define kvm::guest (
   String $image,
   Hash[String, Hash] $disks,
+  Integer $ram = 1024,
+  Integer $cores = 1,
   String $vm_name = $title,
 ) {
   $disks.each | String $name, Hash $options | {
@@ -16,10 +20,23 @@ define kvm::guest (
 
   $image_path = "${kvm::image_path}/${image}"
   $root_device = "/dev/${disks['root']['vg']}/guest-${vm_name}-root"
+  $vnc_path = "${kvm::console_path}/vnc/$vm_name.sock"
+  $serial_path = "${kvm::console_path}/serial/$vm_name.sock"
+  $monitor_path = "${kvm::console_path}/monitor/$vm_name.sock"
 
   exec { "Create root for ${root_device}":
     command => "/usr/bin/qemu-img convert -O raw --target-is-zero -n '${image_path}' '${root_device}'",
     onlyif  => "/usr/bin/file -sLb '${root_device}' | grep '^data$'",
     require => Disks::Lv["guest-${vm_name}-root"],
+  }
+
+  -> file { "/etc/kvm/${vm_name}":
+    ensure  => file,
+    content => template('kvm/guest.conf.erb')
+  }
+
+  ~> service { "/guest-${vm_name}":
+    ensure => running,
+    enable => true,
   }
 }
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,14 +1,26 @@
 # @summary Configure KVM hosting
 #
 # @param image_path sets the base directory for image storage
+# @param console_path sets the location for QEMU/serial sockets
 # @param images sets the OS images to use for bootstrapping VMs
 # @param guests sets guest VM information for management
 class kvm (
   String $image_path,
+  String $console_path = '/var/lib/kvm',
   Hash[String, Hash] $images = [],
   Hash[String, Hash] $guests = {},
 ) {
-  package { 'qemu-headless': }
+  package { [
+    'qemu-headless',
+    'socat',
+  ]: }
+
+  file { [$console_path, "${console_path}/vnc", "${console_path}/serial",  "${console_path}/monitor"]:
+    ensure => directory,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
+  }
 
   $images.each | String $name, Hash $options | {
     kvm::image { $name:
@@ -20,5 +32,17 @@ class kvm (
     kvm::guest { $name:
       * => $options,
     }
+  }
+
+  file { '/etc/kvm':
+    ensure => directory,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
+  }
+
+  file { '/etc/systemd/system/kvm@.service':
+    ensure => file,
+    source => 'puppet:///modules/kvm/kvm@.service',
   }
 }
