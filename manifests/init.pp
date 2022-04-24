0acfc352e323b6c95c7f0dec7c6f7ea3dad19bbf
Les Aker <me@lesaker.org>
2022-04-23 22:50:40 -0400

    add image support

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,26 @@
 # @summary Configure KVM hosting
 #
+# @param image_path sets the base directory for image storage
+# @param images sets the OS images to use for bootstrapping VMs
+# @param guests sets guest VM information for management
 class kvm (
+  String $image_path,
+  Hash[String, String] $images = [],
+  Hash[String, Hash] $guests = {},
 ) {
   package { 'qemu-headless': }
+
+  $images.each | String $name, Hash $options | {
+    file { "${image_path}/${name}":
+      ensure   => file,
+      source   => $options['url'],
+      checksum => $options['checksum'],
+      mode     => '0644',
+      owner    => 'root',
+      group    => 'root',
+    }
+  }
+
+  $guests.each | String $name, Hash $options | {
+  }
 }
