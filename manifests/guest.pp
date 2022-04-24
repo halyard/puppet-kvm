11e76ef1b9e720bfc7fbb73696b018ec69bfefa9
Les Aker <me@lesaker.org>
2022-04-23 23:41:10 -0400

    do more guest stuff

--- manifests/guest.pp
+++ manifests/guest.pp
@@ -1,7 +1,21 @@
 # @summary Defines a guest VM
 #
+# @param image sets the system image to use to bootstrap the guest
+# @param disks sets the LVs to create for the guest
 # @param vm_name sets the name of the VM
 define kvm::guest (
+  String $image,
+  Hash[String, Hash] $disks,
   String $vm_name = $title,
 ) {
+  $disks::lvs.each | String $name, Hash $options | {
+    disks::lv { "guest-${vm_name}-${name}":
+      * => $options,
+    }
+  }
+
+  exec { "/usr/bin/qemu-img convert -O raw '${kvm::image_path}/${image}' '/dev/${vg}/guest-${vm_name}-root'":
+    onlyif  => "/usr/bin/file -sLb '/dev/${vg}/${lvname}' | grep '^data$'",
+    require => Disks::Lv["guest-${vm_name}-root"],
+  }
 }
